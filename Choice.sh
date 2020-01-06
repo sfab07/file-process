d1e89a36112ba3d333a3a8fdfa47e0a6b0ae9c62
sfab07 <sfab07@gmail.com>
2020-01-06 16:02:05 +0100

    Create Choice.sh

---
+++ Choice.sh
@@ -0,0 +1,4 @@
+yad --center --fixed --borders=0 --button='!s1.png:"./Traitements PDF.sh"' \
+--button='!s2.png:"./PDF concatenation.sh"' \
+--button='!s3.png:"./Traitements MP3.sh"' \
+--button='!s4.png:"./Traitements x265.sh"' \
